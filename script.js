4b25a2b3719ac9e661d615ea4f917afe6c107986
Arsenic-01 <vedbhor25@gmail.com>
2024-02-08 21:02:21 +0530

    title logo and title

--- script.js
+++ script.js
@@ -19,3 +19,13 @@ let calcScrollValue = () => {
 
 window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
